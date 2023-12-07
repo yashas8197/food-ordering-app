29a0caa96ba0d8375ba90cba81696ddbb75001a7
yashas8197 <yashasjr7@gmail.com>
2023-12-07 10:32:29 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "This is Namaste React 🚀"),
+      React.createElement("h2", {}, "by Akshay Saini"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+  ]);
+  
+  console.log(parent); // object
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
